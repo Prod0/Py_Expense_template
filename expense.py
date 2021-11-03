1e2d54d30b6357291f520ec5d8ee21f4381f4953
Ruben Courbin <Prod0@MacBook-Pro-de-Ruben.local>
2021-11-03 22:01:38 +0100

    v final

--- expense.py
+++ expense.py
@@ -1,4 +1,26 @@
 from PyInquirer import prompt
+import csv
+import os
+import sys
+
+
+def readUsers(infos):
+    result = []
+    with open('users.csv', 'r', newline='') as csvfile:
+        spamreader = csv.DictReader(csvfile)
+        for row in spamreader:
+            result.append(row['User'])
+    return result
+
+def readUsersCheckbox(infos):
+    result = []
+    with open('users.csv', 'r', newline='') as csvfile:
+        spamreader = csv.DictReader(csvfile)
+        for row in spamreader:
+            result.append({"name":row['User']})
+    return result
+
+    
 
 expense_questions = [
     {
@@ -12,19 +34,58 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": readUsers,
+    },
+    {
+        "type":"checkbox",
+        "name":"InvolvedPeople",
+        "message":"New Expense - Involved People: ",
+        "choices": readUsersCheckbox,
     },
 
 ]
 
 
+# method writing informations in CSV of the expense
+def pushInCSV(infos):
+    file_exists = os.path.isfile('expense_report.csv')
+    with open('expense_report.csv', 'a', newline='') as csvfile:
+        headers = ['Amount', 'Label', 'Spender', 'InvolvedPeople']
+        spamwriter = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+        if not file_exists:
+            spamwriter.writeheader()  # file doesn't exist yet, write a header
+        spamwriter.writerow({'Amount': infos.get('amount'), 'Label': str(infos.get('label')), 'Spender': str(infos.get('spender')), "InvolvedPeople": infos.get('InvolvedPeople')})
+        
+def repartition(infos):
+    jsp = infos.get('InvolvedPeople')
+    taille = len(jsp)
+    montant = int(infos.get('amount')) / taille
+    for elt in jsp:
+        print(elt + " has to pay " + str(montant))
+
+
 
 def new_expense(*args):
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    try:
+        int(infos.get('amount'))
+    except ValueError:
+        print("Oops!  That was no valid number for the amount.  Try again...")
+        sys.exit(1)
+
+    if (infos.get('label').isdigit()):
+        print("Oops!  That was no valid type for the label.  Try again with string")
+        sys.exit(1)
+
+    repartition(infos)
+
+    pushInCSV(infos)
+
     print("Expense Added !")
+
     return True
 
 
--- user.py
+++ user.py
@@ -1,8 +1,33 @@
 from PyInquirer import prompt
-user_questions = [
+import os
+import csv
+import sys
 
+user_questions = [
+ {
+        "type":"input",
+        "name":"name",
+        "message":"New User - Name: ",
+    },
 ]
 
+# method writing informations in CSV of the expense
+def pushInCSV(infos):
+    file_exists = os.path.isfile('users.csv')
+    with open('users.csv', 'a', newline='') as csvfile:
+        headers = ['User']
+        spamwriter = csv.DictWriter(csvfile, delimiter=',', lineterminator='\n',fieldnames=headers)
+        if not file_exists:
+            spamwriter.writeheader()  # file doesn't exist yet, write a header
+        spamwriter.writerow({'User': str(infos.get('name'))})
+
 def add_user():
-    # This function should create a new user, asking for its name
-    return
+    infos = prompt(user_questions)
+
+    if (infos.get('name').isdigit()):
+        print("Oops!  That was no valid type for the user.  Try again with string")
+        sys.exit(1)
+    pushInCSV(infos)
+    print("User Added !")
+    
+    return True
